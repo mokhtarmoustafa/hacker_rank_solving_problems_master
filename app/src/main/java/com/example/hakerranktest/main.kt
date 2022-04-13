3410e0ce34c660d50b6acd7e125880fb99ac9b0d
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-04-13 07:32:08 +0200

    solve2

--- app/src/main/java/com/example/hakerranktest/main.kt
+++ app/src/main/java/com/example/hakerranktest/main.kt
@@ -6,7 +6,7 @@ import com.example.hakerranktest.problem_solving.BirthdayCakeCandles
 fun main(args: Array<String>) {
 
     var array = arrayOf(4, 4, 4, 3)
-    val count = BirthdayCakeCandles().solve1(array)
+    val count = BirthdayCakeCandles().solve2(array)
 
     print(count.toString())
 
--- app/src/main/java/com/example/hakerranktest/problem_solving/BirthdayCakeCandles.kt
+++ app/src/main/java/com/example/hakerranktest/problem_solving/BirthdayCakeCandles.kt
@@ -5,9 +5,14 @@ class BirthdayCakeCandles {
 
     fun solve1(array: Array<Int>): Int {
         array.sortDescending()
-        var first=array[0]
-        var count=array.count { it ==first}
+        var first = array[0]
+        var count = array.count { it == first }
         return count
 
+    }
+
+    fun solve2(array: Array<Int>): Int {
+        val max = array.maxOrNull()
+        return array.count { it == max }
     }
 }
